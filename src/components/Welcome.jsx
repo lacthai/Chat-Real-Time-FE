f712fa4f046fb6019be1c75b641e1b974d816189
Thai Gia Lac <lacthai08@gmail.com>
2023-06-04 20:56:51 +0700

    updated FE

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Hello from "../assets/hello.gif";
-import Logout from "./Logout";
+
 
 const Welcome = () => {
   const [userName, setUserName] = useState("");
